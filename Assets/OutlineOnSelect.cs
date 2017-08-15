31e2bec15de2cbc82961051eb89293ba318b97e6
Sebastian Gambolati <sgambolati@gmail.com>
2017-08-15 10:11:20 -0300

    Change the way to select a hologram.
    Delete the Raycast way and use the `IFocusable` interface.

--- Assets/OutlineOnSelect.cs
+++ Assets/OutlineOnSelect.cs
@@ -1,63 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
 using cakeslice;
+using HoloToolkit.Unity.InputModule;
 using UnityEngine;
 
-public class OutlineOnSelect : MonoBehaviour
+public class OutlineOnSelect : MonoBehaviour, IFocusable
 {
+    private Outline outlineComponent;
 
-    // Use this for initialization
     void Start()
     {
+        this.outlineComponent = this.GetComponent<Outline>();
 
+        // Disable the component on start. 
+        OnFocusExit(); 
     }
 
-    private GameObject FocusedObject;
-
-    // Update is called once per frame
-    void Update()
+    public void OnFocusEnter()
     {
-        // Figure out which hologram is focused this frame.
-        GameObject oldFocusObject = FocusedObject;
-
-        // Do a raycast into the world based on the user's
-        // head position and orientation.
-        var headPosition = Camera.main.transform.position;
-        var gazeDirection = Camera.main.transform.forward;
-
-        RaycastHit hitInfo;
-        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        if (this.outlineComponent != null)
         {
-            // If the raycast hit a hologram, use that as the focused object.
-            FocusedObject = hitInfo.collider.gameObject;
+            this.outlineComponent.enabled = true;
         }
-        else
-        {
-            // If the raycast did not hit a hologram, clear the focused object.
-            FocusedObject = null;
-        }
+    }
 
-        if (FocusedObject != oldFocusObject)
+    public void OnFocusExit()
+    {
+        if (this.outlineComponent != null)
         {
-            // Deselect the old selected GameObject is not null
-            if (oldFocusObject != null)
-            {
-                var outlineComponent = oldFocusObject.GetComponent<Outline>();
-                if (outlineComponent != null)
-                {
-                    outlineComponent.enabled = false;
-                }
-            }
-
-            // Select the selected GameObject is not null
-            if (FocusedObject != null)
-            {
-                var outlineComponent = FocusedObject.GetComponent<Outline>();
-                if (outlineComponent != null)
-                {
-                    outlineComponent.enabled = true;
-                }
-            }
+            this.outlineComponent.enabled = false;
         }
     }
 }
